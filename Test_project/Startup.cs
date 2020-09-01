5cdbaf53c018c289cc0c6187ac0387ae78240df2
venkat <vsaaddula@dxc.com>
2020-09-01 16:05:20 +0530

    added edited file

--- Test_project/Startup.cs
+++ Test_project/Startup.cs
@@ -49,7 +49,7 @@ namespace Test_project
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();dddd
+                endpoints.MapRazorPages();
             });
         }
     }
